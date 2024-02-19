7d5f1793ddea96a8076c8f0aeb4bd1f9fd95a04d
Lucas Freire <lucasfreiredev1@gmail.com>
2024-02-19 19:05:24 -0300

    Created base html created login and cadastrar html, coded cadastrar html, created login and cadastrar urls

--- sessions/settings.py
+++ sessions/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'usuarios',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'sessions.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'sessions/templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- sessions/urls.py
+++ sessions/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('usuario.urls'))
+    path('auth/', include('usuarios.urls')),
+    path('', lambda request: redirect('/auth/login')),
 ]
